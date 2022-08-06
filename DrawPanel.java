7b67029e87e5d818107eb49b177aacfa1596a0ba
Matz <mateus.assalti@gmail.com>
2022-08-06 11:31:42 -0300

    Removed comparations tests

--- DrawPanel.java
+++ DrawPanel.java
@@ -17,16 +17,6 @@ public class DrawPanel extends JPanel {
 
   @Override
   public void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    // Comparing funcitons
-    g.drawLine(20, 1, 110, 400);
-    LineGr line = new LineGr(30, 1, 120, 400);
-    line.drawLine(g);
-
-    LineGr line1 = new LineGr(300, 555, 500, 555);
-    line1.drawLine(g);
-
-    g.drawLine(300, 655, 500, 655);
 
   }
 }
